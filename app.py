9820f1344f0915031b4e3122e3c28b2df4d344a8
Ulisses Mariano <110139513+UlissesMariano@users.noreply.github.com>
2023-11-26 14:02:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ import streamlit as st
 import pandas as pd 
 import datetime
 import re
+# C:\Users\Ulisses\Desktop\Backup\1. ESTUDOS\A PARTE\PROJETOS\STREAMLIT
 
 arquivo_dados = 'dados/clientes.csv'
 
 
 # INICIO PROGRAMA :
-st.title('Seu programa de cadastro !')
+st.title('Dottore Podologia')
 
-login = st.text_input('Login')
+
+login = st.text_input('Login').lower() # VERIFICAR COM ELE ******************
 password = st.text_input('Password', type='password')
 
+
 # LOGIN ADMIN **
-if (login == 'admin' and password == 'admin'):
+if (login == 'elando' and password == 'elando'):
     st.success(f'Logged {login} account')
     
     # DEPOIS DE LOGAR - TASKS DO SIDEBAR :
@@ -33,37 +36,50 @@ if (login == 'admin' and password == 'admin'):
         # MARCAR CONSULTA :
         if tasks2 == 'Marcar Consulta' :
             
-            st.subheader("Marcar Consulta")
+            st.subheader("Dados da consulta")
 
             with st.form('Marcar Consulta'):
-
+                
+                # CONTAINER 1 : dados pessoais
                 c1 = st.container()
                 a, b, c = c1.columns([4,2,2])
+
                 nome = a.text_input('Nome').upper()
                 tel = b.text_input('Telefone')
                 dt_nascimento = c.date_input('Data de Nascimento'
                                             , value=datetime.date(1997,1,1)
                                             , min_value=datetime.date(1900,1,1)
-                                            , max_value=datetime.date.today())
-
-
-                c2 = st.container()
-                aa, bb = c2.columns([6,2])
-                endereco = aa.text_input('Endereço')
-                nro_endereco = bb.number_input('Nro', step=1)
+                                            , max_value=datetime.date.today()
+                                            , format="DD/MM/YYYY")
+                dt_nascimento = dt_nascimento.strftime("%d/%m/%Y")
                 email = st.text_input('Email')
 
 
+                # CONTAINER 2 : dados de endereço
+                c2 = st.container()
+                aa, bb = c2.columns([4,4])
+
+                logradouro = aa.text_input('Logradouro')
+                nro_endereco = bb.number_input('Nº', step=1)
+                bairro = aa.text_input('Bairro')
+                cidade = bb.text_input('Cidade')
+
+
+                # CONTAINER 3 : dados da consulta
                 c3 = st.container()
                 aaa, bbb = c3.columns([6,2])
+
                 profissional = aaa.text_input('Profissional').upper()
-                dt_atendimento = bbb.date_input('Data de Atendimento')
+                dt_atendimento = bbb.date_input('Data de Atendimento', format="DD/MM/YYYY")
+                dt_atendimento = dt_atendimento.strftime("%d/%m/%Y")
+
                 como_nos_encontrou = st.text_input('Como nos encontrou ?')
                 obs = st.text_area('Observação')
-                
+
 
                 if st.form_submit_button('Adicionar'): 
-                    lista = [nome, tel, email, dt_nascimento, endereco, nro_endereco, profissional, dt_atendimento, como_nos_encontrou, obs]
+                    lista = [nome, tel, email, dt_nascimento, logradouro, nro_endereco, bairro
+                             , cidade, profissional, dt_atendimento, como_nos_encontrou, obs]
                     dados = pd.read_csv(arquivo_dados)
                     ultima_linha = dados.shape[0]
                     dados.loc[ultima_linha] = lista
@@ -72,6 +88,7 @@ if (login == 'admin' and password == 'admin'):
                     dados.to_csv(arquivo_dados, index=False)
                     dados_atualizados = pd.read_csv(arquivo_dados)
                     st.write('Adicionado com sucesso !')
+
 
         # BUSCAR PACIENTE :
         elif tasks2 == 'Buscar Paciente':
@@ -89,7 +106,7 @@ if (login == 'admin' and password == 'admin'):
             busca_nome = st.sidebar.text_input('Buscar pelo nome').upper()
             if st.sidebar.button('Buscar pelo nome'):
                 st.subheader('Ficha do Paciente')
-                st.dataframe(dados[dados['tel'] == busca_nome])
+                st.dataframe(dados[dados['nome'] == busca_nome])
                 
 
             if busca_nome == '' and busca_tel == '': 
